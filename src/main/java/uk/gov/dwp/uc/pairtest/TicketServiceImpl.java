cc9c187c02c81416d4b37fe622c6d04bb7a80bcc
Brian Klonder <bkxverion@gmail.com>
2023-02-10 09:59:19 +0000

    initial commit

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -0,0 +1,98 @@
+package uk.gov.dwp.uc.pairtest;
+
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import java.util.Arrays;
+
+public class TicketServiceImpl implements TicketService {
+    /**
+     * Should only have private methods other than the one below.
+     */
+    private TicketPaymentServiceImpl ticketPaymentService;
+    private SeatReservationServiceImpl seatReservationService;
+
+    public TicketServiceImpl(TicketPaymentServiceImpl ticketPaymentService, SeatReservationServiceImpl seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
+    }
+
+    @Override
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        validateInputs(accountId, ticketTypeRequests);
+
+        int cost = calculateCost(ticketTypeRequests);
+        int seats = calculateSeats(ticketTypeRequests);
+
+        processPayment(accountId, cost);
+        reserveSeats(accountId, seats);
+    }
+
+    private void validateInputs(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        if (ticketTypeRequests == null || ticketTypeRequests.length == 0)
+            throw new InvalidPurchaseException("No valid ticket type requests provided");
+        if (accountId == null || accountId <= 0)
+            throw new InvalidPurchaseException("Invalid account id provided");
+
+        boolean isAdultPresent = false;
+        int ticketAmount = 0;
+
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+            ticketAmount += ticketTypeRequest.getNoOfTickets();
+            if (ticketAmount > 20)
+                throw new InvalidPurchaseException("Too many tickets being purchased at once, please lower the number of tickets");
+            if (ticketTypeRequest.getTicketType() == TicketTypeRequest.Type.ADULT)
+                isAdultPresent = true;
+        }
+
+        if (!isAdultPresent)
+            throw new InvalidPurchaseException("No adult tickets are present, Please add a adult ticket");
+    }
+
+    private int calculateCost(TicketTypeRequest... ticketTypeRequests) {
+        int cost = 0;
+
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+            switch (ticketTypeRequest.getTicketType()) {
+                case CHILD:
+                    cost += ticketTypeRequest.getNoOfTickets() * 10;
+                    break;
+                case ADULT:
+                    cost += ticketTypeRequest.getNoOfTickets() * 20;
+                    break;
+            }
+        }
+
+        return cost;
+    }
+
+    private int calculateSeats(TicketTypeRequest... ticketTypeRequests) {
+        int[] seats = new int[3];
+        Arrays.stream(ticketTypeRequests).forEach(ttr -> seats[ttr.getTicketType().ordinal()] += ttr.getNoOfTickets());
+
+        if (seats[TicketTypeRequest.Type.INFANT.ordinal()] > seats[TicketTypeRequest.Type.ADULT.ordinal()])
+            throw new InvalidPurchaseException("Too many infant tickets, there should be at least 1 adult for every infant");
+
+        return seats[TicketTypeRequest.Type.ADULT.ordinal()] + seats[TicketTypeRequest.Type.CHILD.ordinal()];
+    }
+
+    private void processPayment(Long accountId, int cost) throws InvalidPurchaseException {
+        try {
+            ticketPaymentService.makePayment(accountId, cost);
+        } catch (Exception e) {
+            throw new InvalidPurchaseException(e.getMessage());
+        }
+    }
+
+    private void reserveSeats(Long accountId, int seats) throws InvalidPurchaseException {
+        try {
+            seatReservationService.reserveSeat(accountId, seats);
+        } catch (Exception e) {
+            throw new InvalidPurchaseException(e.getMessage());
+        }
+    }
+
+
+}
---
+++ src/test/java/TicketServiceImplTest.java
@@ -0,0 +1,94 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import thirdparty.paymentgateway.TicketPaymentServiceImpl;
+import thirdparty.seatbooking.SeatReservationServiceImpl;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class TicketServiceImplTest {
+
+    @Mock
+    private TicketPaymentServiceImpl ticketPaymentService;
+
+    @Mock
+    private SeatReservationServiceImpl seatReservationService;
+
+    @InjectMocks
+    private TicketServiceImpl ticketService;
+
+    @Test
+    public void testPurchaseTicketsSuccess() throws InvalidPurchaseException {
+        TicketTypeRequest[] ticketTypeRequests = new TicketTypeRequest[]{
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 3),
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 2),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2)
+        };
+
+        ticketService.purchaseTickets(1L, ticketTypeRequests);
+
+        verify(ticketPaymentService, times(1)).makePayment(1L, 80);
+        verify(seatReservationService, times(1)).reserveSeat(1L, 5);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchaseTicketsInvalidInputs() throws InvalidPurchaseException {
+        TicketTypeRequest[] ticketTypeRequests = new TicketTypeRequest[0];
+
+        ticketService.purchaseTickets(1L, ticketTypeRequests);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchaseTicketsNoAdultTicket() throws InvalidPurchaseException {
+        TicketTypeRequest[] ticketTypeRequests = new TicketTypeRequest[]{
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2)
+        };
+
+        ticketService.purchaseTickets(1L, ticketTypeRequests);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testPurchaseTicketsTooManyTickets() throws InvalidPurchaseException {
+        TicketTypeRequest[] ticketTypeRequests = new TicketTypeRequest[]{
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 15),
+                new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 7)
+        };
+
+        ticketService.purchaseTickets(1L, ticketTypeRequests);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void testInvalidAccountId() throws InvalidPurchaseException {
+        ticketService.purchaseTickets(null, new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 1));
+
+        verifyNoInteractions(ticketPaymentService);
+        verifyNoInteractions(seatReservationService);
+    }
+
+    @Test(expected = InvalidPurchaseException.class)
+    public void purchaseTickets_moreInfantsThanAdults_throwsException() throws InvalidPurchaseException {
+        TicketTypeRequest[] ticketTypeRequests = {
+                new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 5),
+                new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 4)
+        };
+
+        try {
+            ticketService.purchaseTickets(1L, ticketTypeRequests);
+            fail("Expected InvalidPurchaseException");
+        } catch (InvalidPurchaseException e) {
+            verify(ticketPaymentService, never()).makePayment(anyLong(), anyInt());
+            verify(seatReservationService, never()).reserveSeat(anyLong(), anyInt());
+            throw e;
+        }
+    }
+
+}
